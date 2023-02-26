8c62dd24a3134cef834cb77de06a1e4838ec5df7
Passion-cyber <chineduibelachi@gmail.com>
2023-02-27 00:31:51 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import API from './components/API'
 
 function App() {
   return (
+    <>
+    <h2 className='header-text'>🔍 Emoji Search App 🔎</h2>
     <div className="container">
      <API/>
     </div>
+    </>
   );
 }
 
